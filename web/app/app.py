c0d943ca72383bb967299e6815ca1958d1c21b64
Sean <seanmarman@gmail.com>
2020-03-27 21:10:39 -0700

    keep up-to-date

--- web/app/app.py
+++ web/app/app.py
@@ -6,5 +6,23 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
+@app.route("/health", methods = ['GET'])
+def healthcheck():
+    data = {
+            'healthy' : True
+            }
+    resp = jsonify(data)
+    resp.status_code = 200
+    resp.headers['Link'] = 'https://gasthaus.marqeta.com'
+    return resp
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template('error.html')
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return "page not found"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False)
